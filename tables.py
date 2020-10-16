e7fe6db92d254b6f8e29fcf74d43d3b669881b53
Adam A <aaa237@njit.edu>
2020-10-16 19:09:09 +0000

    Increase char limit for pictureURL because 120 is too small

--- tables.py
+++ tables.py
@@ -6,10 +6,12 @@ class Chat_log(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(120))
     user = db.Column(db.String(120))
+    pictureURL = db.Column(db.String(240))
     
-    def __init__(self, a, b):
-        self.content = a
-        self.user = b
+    def __init__(self, content, user, pictureURL):
+        self.content = content
+        self.user = user
+        self.pictureURL = pictureURL
         
     def __repr__(self):
         return '<Message content: %s>' % self.content
@@ -19,12 +21,14 @@ class AuthUser(db.Model):
     auth_type = db.Column(db.String(120))
     name = db.Column(db.String(120))
     email = db.Column(db.String(120), unique=True)
+    pictureURL = db.Column(db.String(240))
     
-    def __init__(self, name, auth_type, email):
+    def __init__(self, name, auth_type, email, pictureURL):
         assert type(auth_type) is AuthUserType
         self.name = name
         self.auth_type = auth_type.value
         self.email = email
+        self.pictureURL = pictureURL
         
     def __repr__(self):
         return "<User name: {}\ntype: {}".format(self.name, self.auth_type)
