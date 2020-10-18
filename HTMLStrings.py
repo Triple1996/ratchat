3ca4c3c554b0df73843c9f30377965b9b6c1022c
Adam A <aaa237@njit.edu>
2020-10-18 16:38:51 +0000

    Use rfc3987 from PyPi to parse URLs. If failed, try once more with prefix 'https://'

--- HTMLStrings.py
+++ HTMLStrings.py
@@ -1,18 +1,33 @@
 # take a list of messages and convert to <img>, <a>, or just leave as text
+from rfc3987 import parse
+
 class HTMLStrings:
     
     def __init__(self):
         self.HTMLWriter = "HTMLWriter"
-        
+    
+    def isHTML(self, message):
+        try:
+            parse(message, rule='URI')
+            return True
+        except ValueError:
+            return False
+            
     def formatHTML(self, messages):
         imageExtensions = ['.jpg', '.png', '.gif']
         urlExtensions = ['.com', '.org', '.net', '.gov', '.edu']
+        
+        
         i = 0
         for message in messages:
             if (message[-4:].lower() in imageExtensions):
                 messages[i] = "<img src={} className='pictures' height=45px;/>".format(messages[i])
-            elif (message[-4:].lower() in urlExtensions):
+            elif (self.isHTML(message)):
                 messages[i] = "<a href={}>{}</a>".format(messages[i], messages[i])
             else:
-                pass
+                temp="https://"+message
+                if (self.isHTML(temp)):
+                    messages[i] = "<a href={}>{}</a>".format(temp, messages[i])
             i+=1
+            
+    
