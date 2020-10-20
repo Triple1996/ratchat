a02fca4816468c9b05344bd886cf38d0ae737558
Adam A <aaa237@njit.edu>
2020-10-20 18:09:09 +0000

    Place all message elements (content, signature, picture) inside one structure, remove redundant loops

--- scripts/Content.jsx
+++ scripts/Content.jsx
@@ -26,19 +26,13 @@ export function Content() {
                     <span>Logged in user count: {userCount}</span>
                 </div>
                 <div id='chat-container'>
-                    <div id='messages-wrapper'>
                         {messages.map((message, index) =>
-                            <ul className="chat-items" key={index} dangerouslySetInnerHTML={{__html: `${message[0]}` }}></ul> )}
-                    </div>
-                    <div id='pics-wrapper'>
-                        {messages.map((pics, index) =>
-                            <ul className ='pic-items'key={index}><img src={pics[2]} className="pictures" /></ul> )}
-                    </div>
-                    <div id='users-wrapper'>
-                        {messages.map((sign, index) =>
-                            <ul className="signatures" key={index}>{sign[1]}</ul>)}
-                        <ul id="anchor"></ul>
-                    </div>
+                            <ul id='messages-wrapper' key={index}>
+                                <div className="chat-items" dangerouslySetInnerHTML={{__html: `${message[0]}` }}></div>
+                                <div className ="pic-items"><img src={message[2]} className="pictures" /></div>
+                                <div className="signatures">{message[1]}</div>
+                            </ul>
+                        )}
                 </div>
             <InputField />
             <GoogleButton />
