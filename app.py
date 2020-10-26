cc44d3d894963062edcb1a71eedf2c867b725e95
Adam A <aaa237@njit.edu>
2020-10-26 20:16:17 +0000

    Make changes according to pylint conventions

--- app.py
+++ app.py
@@ -1,49 +1,47 @@
-# app.py
+"""
+ this is the doctring
+"""
 from os.path import join, dirname
 import dotenv
 import os
 import flask
 import flask_sqlalchemy
 import flask_socketio
-import requests
-import json
-import random
 from verminbot import Verminbot
-from HTMLStrings import HTMLStrings
 from HTMLStrings import HTMLStrings
 
 MESSAGES_RECEIVED_CHANNEL = "messages received"
 
-app = flask.Flask(__name__)
+APP = flask.Flask(__name__)
 
-socketio = flask_socketio.SocketIO(app)
-socketio.init_app(app, cors_allowed_origins="*")
+SOCKETIO = flask_socketio.SocketIO(APP)
+SOCKETIO.init_app(APP, cors_allowed_origins="*")
 
 try:
-    dotenv_path = join(dirname(__file__), "sql.env")
-    dotenv.load_dotenv(dotenv_path)
-except Exception as e:
-    print("Handled error: " + str(e))
+    DOTENV_PATH = join(dirname(__file__), "sql.env")
+    dotenv.load_dotenv(DOTENV_PATH)
+except ImportError as error:
+    print("Handled error: " + str(error))
 
-database_uri = os.environ["DATABASE_URL"]
+DATABASE_URI = os.environ["DATABASE_URL"]
 
-app.config["SQLALCHEMY_DATABASE_URI"] = database_uri
+APP.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
 
-db = flask_sqlalchemy.SQLAlchemy(app)
-db.init_app(app)
-db.app = app
+DB = flask_sqlalchemy.SQLAlchemy(APP)
+DB.init_app(APP)
+DB.app = APP
 
-db.create_all()
-db.session.commit()
+DB.create_all()
+DB.session.commit()
 
 import tables
 
 # list of current users
-userIndex = {}
-bot = Verminbot()
-HTMLWriter = HTMLStrings()
-botPic = "https://cdn-images-1.medium.com/max/800/1*ktXRqt9UHhJf3miHG3zpvQ.png"
-botSign = "@VERMINBOT"
+USER_INDEX = {}
+BOT = Verminbot()
+HTML_WRITER = HTMLStrings()
+BOT_PIC = "https://cdn-images-1.medium.com/max/800/1*ktXRqt9UHhJf3miHG3zpvQ.png"
+BOT_SIGN = "@VERMINBOT"
 
 
 def emit_all_messages(channel):
@@ -53,104 +51,104 @@ def emit_all_messages(channel):
     all_pics = []
 
     for db_message, db_user, db_pic in (
-        db.session.query(
-            tables.Chat_log.content, tables.Chat_log.user, tables.Chat_log.pictureURL
-        )
-        .order_by(tables.Chat_log.id.desc())
-        .limit(50)
-        .all()
-    ):
+            DB.session.query(
+                tables.Chat_log.content, tables.Chat_log.user, tables.Chat_log.pictureURL
+            )
+            .order_by(tables.Chat_log.id.desc())
+            .limit(50)
+            .all()
+        ):
         all_messages.append(db_message)
         all_signs_log.append(db_user)
         all_pics.append(db_pic)
     all_chat = []
-    HTMLWriter.formatHTML(all_messages)
+    HTML_WRITER.formatHTML(all_messages)
     for i in range(0, len(all_messages)):
         all_chat.append([all_messages[i], all_signs_log[i], all_pics[i]])
 
-    socketio.emit(channel, {"allMessages": all_chat})
+    SOCKETIO.emit(channel, {"allMessages": all_chat})
 
 
-@socketio.on("new message input")
+@SOCKETIO.on("new message input")
 def on_new_message(data):
-    name, pictureURL = (
-        db.session.query(tables.AuthUser.name, tables.AuthUser.pictureURL)
-        .filter(tables.AuthUser.email == userIndex[flask.request.sid])
+    name, picture_url = (
+        DB.session.query(tables.AuthUser.name, tables.AuthUser.pictureURL)
+        .filter(tables.AuthUser.email == USER_INDEX[flask.request.sid])
         .first()
     )
     sign = "Sent by: " + name
     print("Got an event for new message input with data:", data, sign)
-    messageContent = data["message"].strip()
+    message_content = data["message"].strip()
 
     # commit to DB and update everyone's chat
-    db.session.add(tables.Chat_log(data["message"], sign, pictureURL))
-    db.session.commit()
+    DB.session.add(tables.Chat_log(data["message"], sign, picture_url))
+    DB.session.commit()
     emit_all_messages(MESSAGES_RECEIVED_CHANNEL)
 
     # if bot command (first two chars are !!)
-    if messageContent[0] == "!" and messageContent[1] == "!":
-        botRetStr = bot.handle_command(messageContent[2:])
-        db.session.add(tables.Chat_log(botRetStr, botSign, botPic))
-        db.session.commit()
+    if message_content[0] == "!" and message_content[1] == "!":
+        bot_ret_str = BOT.handle_command(message_content[2:])
+        DB.session.add(tables.Chat_log(bot_ret_str, BOT_PIC, BOT_SIGN))
+        DB.session.commit()
         emit_all_messages(MESSAGES_RECEIVED_CHANNEL)
 
 
-@app.route("/")
+@APP.route("/")
 def index():
     emit_all_messages(MESSAGES_RECEIVED_CHANNEL)
     return flask.render_template("index.html")
 
 
-@socketio.on("new google user")
+@SOCKETIO.on("new google user")
 def on_new_google_login(data):
     sid = flask.request.sid
-    userIndex[sid] = data["email"]
+    USER_INDEX[sid] = data["email"]
 
     print("Someone logged in with data: " + str(data) + "\nauth user list:")
 
-    for user in userIndex:
-        print(user + " : " + userIndex[user])
+    for user in USER_INDEX:
+        print(user + " : " + USER_INDEX[user])
 
-    socketio.emit("updateUsers", {"user_count": len(userIndex)})
+    SOCKETIO.emit("updateUsers", {"user_count": len(USER_INDEX)})
 
     try:
-        db.session.add(
+        DB.session.add(
             tables.AuthUser(
                 data["name"], tables.AuthUserType.GOOGLE, data["email"], data["picture"]
             )
         )
-        db.session.commit()
+        DB.session.commit()
     except:  # email already exists in the DB
         pass
 
 
-@socketio.on("connect")
+@SOCKETIO.on("connect")
 def on_connect():
     sid = flask.request.sid
     print("Someone connected with sid: " + sid + "\nauth user list:")
-    for user in userIndex:
-        print(user + " : " + userIndex[user])
+    for user in USER_INDEX:
+        print(user + " : " + USER_INDEX[user])
 
-    socketio.emit("connected", {"test": "Connected"})
+    SOCKETIO.emit("connected", {"test": "Connected"})
 
-    socketio.emit("updateUsers", {"user_count": len(userIndex)})
+    SOCKETIO.emit("updateUsers", {"user_count": len(USER_INDEX)})
 
     emit_all_messages(MESSAGES_RECEIVED_CHANNEL)
 
 
-@socketio.on("disconnect")
+@SOCKETIO.on("disconnect")
 def on_disconnect():
     sid = flask.request.sid
-    userIndex.pop(sid)
+    USER_INDEX.pop(sid)
     print("Someone disconnected with sid: " + sid + "\nauth user list:")
-    for user in userIndex:
-        print(user + " : " + userIndex[user])
-    socketio.emit("updateUsers", {"user_count": len(userIndex)})
+    for user in USER_INDEX:
+        print(user + " : " + USER_INDEX[user])
+    SOCKETIO.emit("updateUsers", {"user_count": len(USER_INDEX)})
 
 
 if __name__ == "__main__":
-    socketio.run(
-        app,
+    SOCKETIO.run(
+        APP,
         host=os.getenv("IP", "0.0.0.0"),
         port=int(os.getenv("PORT", 8080)),
         debug=True,
