ce77310ba7a700ca1c2e65a17c08d649c4a8ddb9
Adam A <aaa237@njit.edu>
2020-10-26 20:22:14 +0000

    Change import

--- tables.py
+++ tables.py
@@ -1,12 +1,12 @@
 import flask_sqlalchemy
-from app import db
+from app import DB
 from enum import Enum
 
-class Chat_log(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200))
-    user = db.Column(db.String(120))
-    pictureURL = db.Column(db.String(200))
+class Chat_log(DB.Model):
+    id = DB.Column(DB.Integer, primary_key=True)
+    content = DB.Column(DB.String(200))
+    user = DB.Column(DB.String(120))
+    pictureURL = DB.Column(DB.String(200))
     
     def __init__(self, content, user, pictureURL):
         self.content = content
@@ -16,12 +16,12 @@ class Chat_log(db.Model):
     def __repr__(self):
         return '<Message content: %s>' % self.content
 
-class AuthUser(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    auth_type = db.Column(db.String(120))
-    name = db.Column(db.String(120))
-    email = db.Column(db.String(120), unique=True)
-    pictureURL = db.Column(db.String(200))
+class AuthUser(DB.Model):
+    id = DB.Column(DB.Integer, primary_key=True)
+    auth_type = DB.Column(DB.String(120))
+    name = DB.Column(DB.String(120))
+    email = DB.Column(DB.String(120), unique=True)
+    pictureURL = DB.Column(DB.String(200))
     
     def __init__(self, name, auth_type, email, pictureURL):
         assert type(auth_type) is AuthUserType
