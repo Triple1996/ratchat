20ebf24910f0f8d1a9718f8c3c9fa191850d7aed
Adam A <aaa237@njit.edu>
2020-10-26 19:19:17 +0000

    Remove default keyword, causing page not to load

--- scripts/GoogleButton.jsx
+++ scripts/GoogleButton.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import GoogleLogin from 'react-google-login';
 import { Socket } from './Socket';
 
+const responseGoogle = (response) => {
+  console.log(response);
+};
+
 function handleSubmit(response) {
+  console.log(response);
   const { name } = response.profileObj;
   const { email } = response.profileObj;
   const profilePicURL = response.profileObj.imageUrl;
@@ -12,16 +18,18 @@ function handleSubmit(response) {
     email,
     picture: profilePicURL,
   });
+  console.log(`logged in with email ${email}!`);
   document.getElementById('chat-input').disabled = false;
   document.getElementById('submit').disabled = false;
 }
 
-export default function GoogleButton() {
+export function GoogleButton() {
   return (
     <GoogleLogin
       clientId="174343683808-a8qlre1saob9enalirddvbkmi4m8al43.apps.googleusercontent.com"
       buttonText="Login to join the chat!"
       onSuccess={handleSubmit}
+      onFailure={responseGoogle}
       cookiePolicy="single_host_origin"
     />
   );
