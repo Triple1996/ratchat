d3f0b48ade6ed2adf36f9ef2ece3b521529715cb
Adam A <aaa237@njit.edu>
2020-10-27 23:24:11 +0000

    Add test cases and upload coverage files

--- tests/mockedunittests.py
+++ tests/mockedunittests.py
@@ -10,7 +10,9 @@ KEY_INPUT = "input"
 KEY_EXPECTED = "expected"
 KEY_CATFACT = "catfact"
 KEY_TRANSLATED = "translated"
-
+LONG_WORD = ""
+for i in range (0, 196):
+    LONG_WORD += 'i'
 class AppTestCase(unittest.TestCase):
     def setUp(self):
 
@@ -28,7 +30,20 @@ class AppTestCase(unittest.TestCase):
                 KEY_TRANSLATED: '"su cuy\'gar ogir"'
             }
         ]
-        
+        self.success_long_translate_params = [
+            {
+                KEY_INPUT: "funtranslate",
+                KEY_EXPECTED: "~/ Translation exceeded message capacity :(",
+                KEY_TRANSLATED: LONG_WORD
+            }    
+        ]
+        self.success_translate_error_params = [
+            {
+                KEY_INPUT: "funtranslate",
+                KEY_EXPECTED: "~/ Too many translations, try again later"
+            }
+        ]
+
     def mocked_cat_request(self, response):
         json_response_mock = mock.Mock()
         json_response_mock().json.return_value = {"all" : [{"text" : response}] }
@@ -38,6 +53,30 @@ class AppTestCase(unittest.TestCase):
         json_response_mock = mock.Mock()
         json_response_mock().json.return_value = {"contents" : {"translated" : response} }
         return json_response_mock
+    
+    def mocked_random_choice(self, values):
+        return values[0]
+        
+    def mocked_translate_error_request(self):
+        json_response_mock = mock.Mock()
+        json_response_mock().json.return_value = {"KeyError" : "wrong"}
+        return json_response_mock
+        
+    def test_too_many_translations(self):
+        verminbot = Verminbot()
+        for test in self.success_translate_error_params:
+            with mock.patch('requests.get', self.mocked_translate_error_request()):
+                response = verminbot.handle_command(test[KEY_INPUT])
+                expected = test[KEY_EXPECTED]
+                self.assertEqual(response, expected)
+        
+    def test_long_translation(self):
+        verminbot = Verminbot()
+        for test in self.success_long_translate_params:
+            with mock.patch('requests.get', self.mocked_translate_request(test[KEY_TRANSLATED])):
+                response = verminbot.handle_command(test[KEY_INPUT])
+                expected = test[KEY_EXPECTED]
+                self.assertEqual(response, expected)
         
     def test_catfact_success(self):
         verminbot = Verminbot()
@@ -46,7 +85,7 @@ class AppTestCase(unittest.TestCase):
                 response = verminbot.handle_command(test[KEY_INPUT])
                 expected = test[KEY_EXPECTED]
                 self.assertEqual(response, expected)
-   
+                
     def test_translate_success(self):
         verminbot = Verminbot()
         for test in self.success_translate_params:
--- tests/unmockedunittests.py
+++ tests/unmockedunittests.py
@@ -13,7 +13,7 @@ KEY_TRANSLATED = "translated"
 
 class AppTestCase(unittest.TestCase):
     def setUp(self):
-        self.success_tables_params = [
+        self.success_html_writer_params = [
             {
                 KEY_INPUT: ['google.com'],
                 KEY_EXPECTED: "<a href=https://google.com>google.com</a>",
@@ -27,25 +27,11 @@ class AppTestCase(unittest.TestCase):
                 KEY_EXPECTED: "<a href=https://google.com>https://google.com</a>",
             }
         ]
-        self.success_bot_params = [
+        self.success_about_params = [
             {
                 KEY_INPUT: 'about',
                 KEY_EXPECTED: "~/ I am the Verminlord.",
-            },
-            {
-                KEY_INPUT: "help",
-                KEY_EXPECTED: "~/ Commands: !!about; !!catfact; " +
-                        "!!1337 <text>; !!funtranslate <text>; !!help;",
-            },
-            {
-                KEY_INPUT: "1337 the quick brown fox",
-                KEY_EXPECTED: "~/ 7h3 quick br0wn f0x",
-            },
-            {
-                KEY_INPUT: "dasd",
-                KEY_EXPECTED: "~/ That command was unrecognized. " +
-                    "For a list of commands, type !!help",
-            },
+            }
         ]
         self.success_catfact_params = [
             {
@@ -61,7 +47,65 @@ class AppTestCase(unittest.TestCase):
                 KEY_TRANSLATED: '"su cuy\'gar ogir"'
             }
         ]
+        self.success_auth_user_params = [
+            {
+                KEY_INPUT: {
+                    "name": 'Billy',
+                    "email": 'email@google.com',
+                    "picurl": 'url'
+                },
+                KEY_EXPECTED: tables.AuthUser('Billy', tables.AuthUserType.GOOGLE, 'email@google.com', 'url')
+            }
+        ]
+        self.success_chat_log_params = [
+            {
+                KEY_INPUT: {
+                    "content": 'message content',
+                    "user": 'Billy',
+                    "picurl": 'url'
+                },
+                KEY_EXPECTED: tables.ChatLog('message content', 'Billy', 'url')
+            }
+        ]
+        self.success_bot_white_space_params = [
+            {
+                KEY_INPUT: 'about ',
+                KEY_EXPECTED: "~/ I am the Verminlord.",
+            },
+            {
+                KEY_INPUT: ' about ',
+                KEY_EXPECTED: "~/ I am the Verminlord.",
+            },
+        ]
         
+        self.success_leet_command_params = [
+            {
+                KEY_INPUT: "1337 the quick brown fox",
+                KEY_EXPECTED: "~/ 7h3 quick br0wn f0x",
+            },
+            {
+                KEY_INPUT: "1337 Sally sells seashells",
+                KEY_EXPECTED: "~/ 5411y 53115 5345h3115",
+            },
+            {
+                KEY_INPUT: "1337Sally sells seashells",
+                KEY_EXPECTED: "~/ 5411y 53115 5345h3115",
+            }
+        ]
+        self.success_unrecognized_params = [
+            {
+                KEY_INPUT: "dasd",
+                KEY_EXPECTED: "~/ That command was unrecognized. " +
+                    "For a list of commands, type !!help",
+            }
+        ]
+        self.success_help_params = [
+            {
+                KEY_INPUT: "help",
+                KEY_EXPECTED: "~/ Commands: !!about; !!catfact; " +
+                        "!!1337 <text>; !!funtranslate <text>; !!help;",
+            }    
+        ]
     def mocked_cat_request(self, response):
         json_response_mock = mock.Mock()
         json_response_mock().json.return_value = {"all" : [{"text" : response}] }
@@ -71,14 +115,42 @@ class AppTestCase(unittest.TestCase):
         json_response_mock = mock.Mock()
         json_response_mock().json.return_value = {"contents" : {"translated" : response} }
         return json_response_mock
-
-    def test_bot_success(self):
+    
+    def test_help_command(self):
         verminbot = Verminbot()
-        for test in self.success_bot_params:
+        for test in self.success_help_params:
             response = verminbot.handle_command(test[KEY_INPUT])
             expected = test[KEY_EXPECTED]
             self.assertEqual(response, expected)
-
+            
+    def test_unrecognized_command(self):
+        verminbot = Verminbot()
+        for test in self.success_unrecognized_params:
+            response = verminbot.handle_command(test[KEY_INPUT])
+            expected = test[KEY_EXPECTED]
+            self.assertEqual(response, expected)
+    
+    def test_leet_command(self):
+        verminbot = Verminbot()
+        for test in self.success_leet_command_params:
+            response = verminbot.handle_command(test[KEY_INPUT])
+            expected = test[KEY_EXPECTED]
+            self.assertEqual(response, expected)
+    
+    def test_bot_success(self):
+        verminbot = Verminbot()
+        for test in self.success_about_params:
+            response = verminbot.handle_command(test[KEY_INPUT])
+            expected = test[KEY_EXPECTED]
+            self.assertEqual(response, expected)
+            
+    def test_bot_white_space_success(self):
+        verminbot = Verminbot()
+        for test in self.success_bot_white_space_params:
+            response = verminbot.handle_command(test[KEY_INPUT])
+            expected = test[KEY_EXPECTED]
+            self.assertEqual(response, expected)
+            
     def test_catfact_success(self):
         verminbot = Verminbot()
         for test in self.success_catfact_params:
@@ -97,11 +169,28 @@ class AppTestCase(unittest.TestCase):
     
     def test_html_writer_success(self):
         htmlWriter = html_strings.HTMLStrings()
-        for test in self.success_tables_params:
+        for test in self.success_html_writer_params:
             messages = test[KEY_INPUT]
             htmlWriter.format_html(messages)
             expected = test[KEY_EXPECTED]
             self.assertEqual(messages[0], expected)
 
+    def test_auth_user_entry(self):
+        for test in self.success_auth_user_params:
+            response = tables.AuthUser(test[KEY_INPUT]["name"], tables.AuthUserType.GOOGLE, test[KEY_INPUT]["email"], test[KEY_INPUT]["picurl"])
+            expected = self.success_auth_user_params[0][KEY_EXPECTED]
+            self.assertEqual(response.name, expected.name)
+            self.assertEqual(response.auth_type, expected.auth_type)
+            self.assertEqual(response.email, expected.email)
+            self.assertEqual(response.picture_url, expected.picture_url)
+
+    def test_chat_log_entry(self):
+        for test in self.success_chat_log_params:
+            response = tables.ChatLog(test[KEY_INPUT]["content"], test[KEY_INPUT]["user"], test[KEY_INPUT]["picurl"])
+            expected = self.success_chat_log_params[0][KEY_EXPECTED]
+            self.assertEqual(response.user, expected.user)
+            self.assertEqual(response.content, expected.content)
+            self.assertEqual(response.picture_url, expected.picture_url)
+            
 if __name__ == '__main__':
     unittest.main()
